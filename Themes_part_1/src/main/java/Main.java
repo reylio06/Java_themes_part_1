49a251147a35f399feb512c006f3ec9233adc703
cristi_macavei <cristi_macavei06@yahoo.com>
2024-01-06 10:25:23 +0200

    Added Triangle class and updated Main

--- Themes_part_1/src/main/java/Main.java
+++ Themes_part_1/src/main/java/Main.java
@@ -3,11 +3,13 @@ public class Main {
     public static void main(String[] args) {
         Circle mycircle = new Circle("Blue", 5.0);
         Rectangle myrectangle = new Rectangle("Yellow", 15, 10);
+        Triangle mytriangle = new Triangle("Red", 3, 4, 5);
 
         System.out.println("Circle area is: " + mycircle.calculateArea());
         System.out.println("Circle string is: " + mycircle);
         System.out.println("Rectangle area is: " + myrectangle.calculateArea());
         System.out.println("Rectangle string is: " + myrectangle);
-
+        System.out.println("Triangle area is: " + mytriangle.calculateArea());
+        System.out.println("Triangle string is: " + mytriangle);
     }
 }
---
+++ Themes_part_1/src/main/java/Triangle.java
@@ -0,0 +1,29 @@
+package main.java;
+
+public class Triangle extends Shape {
+
+    // Triangle attributes
+    protected double side_value1;
+    protected double side_value2;
+    protected double side_value3;
+
+    // Triangle constructor
+    public Triangle(String color, double side_value1, double side_value2, double side_value3){
+        super(color);
+        this.side_value1 = side_value1;
+        this.side_value2 = side_value2;
+        this.side_value3 = side_value3;
+    }
+
+    @Override
+    public double calculateArea() {
+        double semiperimeter = (side_value1 + side_value2 + side_value3)/2;
+        System.out.println("The semiperimeter is: " + semiperimeter);
+        return Math.sqrt((semiperimeter * (semiperimeter - side_value1) * (semiperimeter - side_value2) * (semiperimeter - side_value3)));
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle color is: " + color + " and the side values are: " + " side_value1: " + side_value1 + " side_value2: " + side_value2 + " side_value3: " + side_value3;
+    }
+}
