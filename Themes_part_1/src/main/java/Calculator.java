5f7f3698fd6186cb7a184faf699483c2e5d9532b
cristi_macavei <cristi_macavei06@yahoo.com>
2024-01-06 11:27:01 +0200

    Added Calculator class used for overloading of method multiply, modified Main to write the output of the multiply methods

---
+++ Themes_part_1/src/main/java/Calculator.java
@@ -0,0 +1,13 @@
+package main.java;
+
+public class Calculator {
+
+    public int multiply(int x, int y, int z){
+        return x * y * z;
+    }
+
+    public double multiply(double x, double y, double z){
+        return x * y * z;
+    }
+}
+
--- Themes_part_1/src/main/java/Main.java
+++ Themes_part_1/src/main/java/Main.java
@@ -1,15 +1,12 @@
 package main.java;
 public class Main {
     public static void main(String[] args) {
-        Circle mycircle = new Circle("Blue", 5.0);
-        Rectangle myrectangle = new Rectangle("Yellow", 15, 10);
-        Triangle mytriangle = new Triangle("Red", 3, 4, 5);
+        Calculator mycalculator = new Calculator();
 
-        System.out.println("Circle area is: " + mycircle.calculateArea());
-        System.out.println("Circle string is: " + mycircle);
-        System.out.println("Rectangle area is: " + myrectangle.calculateArea());
-        System.out.println("Rectangle string is: " + myrectangle);
-        System.out.println("Triangle area is: " + mytriangle.calculateArea());
-        System.out.println("Triangle string is: " + mytriangle);
+        int firsttry = mycalculator.multiply(5,6,7);
+        double secondtry = mycalculator.multiply(5.5, 6.6, 7.7);
+
+        System.out.println("Result 1 is: " + firsttry);
+        System.out.println("Result 2 is: " + secondtry);
     }
 }
